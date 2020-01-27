1d229c4d2f9eee4c194d76a8de133661addaf881
Mihai Maruseac <mihai.maruseac@gmail.com>
2020-01-27 07:59:53 -0800

    Builds, but not complete yet

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,4 +1,12 @@
-import qualified Prelude as P
+{-# LANGUAGE NoImplicitPrelude, PackageImports #-}
+
+module Prelude
+  ( module ExportedPrelude
+  , foldl
+  ) where
+
+import "base" Prelude as ExportedPrelude hiding (foldl)
+import qualified "base" Prelude as P
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
 foldl = P.foldl
